3cfb33ea35049c387e53338cc285ecfbba81af87
Adriaan de Groot <groot@kde.org>
2022-11-05 22:13:30 +0100

    [partition] Convert layoutApply()

--- src/modules/partition/gui/ChoicePage.cpp
+++ src/modules/partition/gui/ChoicePage.cpp
@@ -776,7 +776,7 @@ ChoicePage::doAlongsideApply()
             m_core->layoutApply( dev,
                                  newLastSector + 2,
                                  oldLastSector,
-                                 Config::luksGenerationNames().find(m_config->luksFileSystemType()),
+                                 m_config->luksFileSystemType(),
                                  m_encryptWidget->passphrase(),
                                  candidate->parent(),
                                  candidate->roles() );
@@ -851,7 +851,7 @@ ChoicePage::doReplaceSelectedPartition( const QModelIndex& current )
                     m_core->layoutApply( selectedDevice(),
                                          selectedPartition->firstSector(),
                                          selectedPartition->lastSector(),
-                                         Config::luksGenerationNames().find(m_config->luksFileSystemType()),
+                                         m_config->luksFileSystemType(),
                                          m_encryptWidget->passphrase(),
                                          newParent,
                                          newRoles );
