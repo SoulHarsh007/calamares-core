cdb485339c22d51f14768dda75195146f5f23400
dalto <dalto@fastmail.com>
2023-02-23 16:47:39 -0600

    [partition] Make EncryptWidget::setFilesystem take a FileSystem::Type

--- src/modules/partition/gui/EncryptWidget.h
+++ src/modules/partition/gui/EncryptWidget.h
@@ -45,7 +45,7 @@ public:
      * @brief setFilesystem sets the filesystem name used for password validation
      * @param fs A QString containing the name of the filesystem
      */
-    void setFilesystem( const QString& fs );
+    void setFilesystem( const FileSystem::Type fs );
 
     QString passphrase() const;
 
@@ -62,7 +62,7 @@ private:
     Ui::EncryptWidget* m_ui;
     Encryption m_state;
 
-    QString m_filesystem;
+    FileSystem::Type m_filesystem;
 };
 
 #endif  // ENCRYPTWIDGET_H
