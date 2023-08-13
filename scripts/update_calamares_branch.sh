ba5262aa04e67b8705a53768b8f719565e6941ec
shivanandvp <shivanandvp.oss@gmail.com>
2023-08-13 15:33:07 -0500

    refactor: :zap: Improve the upstream staging branch updating script

--- scripts/update_calamares_branch.sh
+++ scripts/update_calamares_branch.sh
@@ -1,31 +1,18 @@
-#! /usr/bin/env sh
-
-SCRIPT_DIRECTORY="$(dirname -- "$(readlink -f -- "$0")")"
-PROJECT_DIRECTORY="$(dirname -- "$SCRIPT_DIRECTORY")"
-BUILD_DIRECTORY="$PROJECT_DIRECTORY"/build
-
-NUMBER_OF_PROCESSORS="$(nproc)"
-
 set -o xtrace
 
 INITIAL_BRANCH="$(git branch --show-current)"
 
 (
     cd "$PROJECT_DIRECTORY" \
-    && git fetch --all \
-    && {
-        git checkout --track origin/calamares \
-        || git checkout calamares
-    } \
-    && {
-        git remote add upstream https://github.com/calamares/calamares \
-        || {
-            git reset --hard upstream/calamares \
-            && git push --force origin calamares
-        }
-    }    
+        && { 
+            git remote add upstream "https://github.com/calamares/calamares" \
+            || true
+        } \
+        && git fetch --all \
+        && git checkout -B _calamares --track upstream \
+        && git reset --hard upstream
 )
 
-git checkout "$INITIAL_BRANCH"
+git checkout -f "$INITIAL_BRANCH"
 
 set +o xtrace
